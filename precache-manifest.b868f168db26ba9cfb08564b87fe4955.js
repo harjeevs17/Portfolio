d4494550b52820dd8387c1ec7f2b356b63bc1835
hark17 <harjeevs17@gmail.com>
2020-08-25 21:28:29 +0530

    Updates

---
+++ precache-manifest.b868f168db26ba9cfb08564b87fe4955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24fa05d64cb3c3341d206ffe78b2ec2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "48894c0b0f3fc13c7567",
+    "url": "/Portfolio/static/css/main.ad268da6.chunk.css"
+  },
+  {
+    "revision": "b27c3541e23983472cbc",
+    "url": "/Portfolio/static/js/2.9a1a5542.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Portfolio/static/js/2.9a1a5542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48894c0b0f3fc13c7567",
+    "url": "/Portfolio/static/js/main.8912ea6f.chunk.js"
+  },
+  {
+    "revision": "91b76910a194f1775f44",
+    "url": "/Portfolio/static/js/runtime-main.aabff8e9.js"
+  }
+]);
