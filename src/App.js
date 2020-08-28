db279b973bcd7dd808d5453d6aa19a3fd6a5a8cb
hark17 <harjeevs17@gmail.com>
2020-08-29 03:32:27 +0530

    Url fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
-import Layout from "./screens/Layout";
-import { Route } from "react-router-dom";
-
+import Main from "./screens/Main";
+import Skills from "./screens/Skills";
+import Projects from "./screens/Projects";
+import Contact from "./screens/Contact";
+import Particles from "react-particles-js";
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Layout} />
+      <Particles />
+      <Main />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
